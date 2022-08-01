fb603d8ad69f4ddfa24aa2659b9d8e1f493a5ba0
LuschenC <103958876+LuschenC@users.noreply.github.com>
2022-08-01 09:50:50 -0300

    Add files via upload
    O site precisa de multiplos ajustes, feito com pressa e não com a perfeição e carinho que merecia.

---
+++ D&D_site/README.txt
@@ -0,0 +1,11 @@
+Um projeto de site feito para o Bootcamp da DIO, por conta da pressa e desejo de conseguir finalizar o bootcamp, 
+não coloquei tanto esforço e pesquisa quanto poderia, reconheço que meu código CSS é extremadamente pobre, o texto HTML
+planejo por mais elementos, como imagens e link que direcionam a algum site, o que não é dificil mas como disse, o 
+tempo... As imagens nos botões estão terriveis, o alinhamento tenho lá minhas dúvidas se está aceitavel. Me foquei na 
+aplicação de conceitos de que fosse possivel apertar um botão que mudasse o texto, e algo que se parece a uma página web,
+em um ambiente de trabalho isto seria o fim de uma carreira, planejo corrigir isso, perdão para aqueles que tem que 
+olhar para algo tão mal feito, mas todos começamos de algum lugar (mesmo que seria bom que fosse um começo melhor...),
+ainda assim aprendi muito e vi alguns textos auxilio na internet que me ajudaram e fiquei contente de ver como a 
+comunidade de desenvolvedores é ativa, espero um dia poder ajudar alguém assim como fui ajudado.
+
+Do luschen do passado para o do futuro, que provavelmente vai fazer esse site deplorável ser algo decente.
---
+++ D&D_site/assets/Js/dnd.js
@@ -0,0 +1,40 @@
+function mage(){
+    document.getElementById('pText').innerHTML = "Selvagem e enigmático, variado nas formas e funções, o poder da magia atrai estudiosos que buscam dominar seus mistérios. Alguns aspiram ser como deuses, moldando a realidade à sua vontade.";
+}
+function rogue(){
+    document.getElementById('pText').innerHTML = "Muitos ladinos focam na furtividade e trapaça, enquanto outros refinam suas perícias para ajudá-los nas masmorras, como escalada, encontrar e desarmar armadilhas, e abrir fechaduras.";
+}
+function barbarian(){
+        document.getElementById('pText').innerHTML = "Os fortes abraçam a sua natureza selvagem instintos aguçados, fisicalidade primitiva e fúria voraz. Bárbaros ficam desconfortáveis quando estão cercados por muralhas e multidões. Eles crescem na natureza selvagem de suas terras natais: a tundra, selva ou pradarias onde suas tribos vivem e caçam. Os bárbaros se sentem mais vivos em meio ao caos do combate. Eles podem entrar num estado de furor quando sua fúria toma controle, concedendo-lhes força e resiliência sobre-humanas";
+}
+function druid(){
+    document.getElementById('pText').innerHTML = "Os druidas reverenciam a natureza acima de tudo, adquirindo suas magias e outros poderes mágicos, ou da força da natureza per si ou de uma divindade da natureza. Muitos druidas buscam uma espiritualidade mística de união transcendental com a natureza ao invés de se devotarem a uma entidade divina, enquanto outros servem deuses da natureza selvagem, animais ou forças elementais.";
+}
+function bard(){
+    document.getElementById('pText').innerHTML = "palavras e música não são meras vibrações do ar, mas vocalizações com poder próprio. O bardo é um mestre da canção, discurso e a magia contida neles. Os bardos dizem que o multiverso foi criado a partir da palavra, que as palavras dos deuses lhe deram forma, e os ecos dessas Palavras de Criação primordiais ainda ressoam através do cosmos. A música dos bardos é uma tentativa de capitar e aproveitar esses ecos, sutilmente tecidas em suas magias e poderes.";
+}
+function warlock(){
+    document.getElementById('pText').innerHTML = "Um Warlock é definido por um pacto com uma entidade transcendental. Às vezes o relacionamento entre um warlock e seu patrono é como o de um clérigo com sua divindade, apesar de os seres que servem como patronos para os bruxos não serem deuses. Um bruxo poderia liderar um culto dedicado a um príncipe-demônio, um arquidemônio ou uma entidade completamente alienígen";
+}
+function cleric(){
+    document.getElementById('pText').innerHTML = "Magia divina, como o nome sugere, é o poder dos deuses fluindo deles para o mundo mortal. Clérigos são os condutores desse poder, manifestando-o através de efeitos milagrosos. Os deuses não conferem esse poder a qualquer um que o queira, mas apenas àqueles escolhidos para cumprir o chamado.";
+}
+function warrior(){
+    document.getElementById('pText').innerHTML = "Guerreiros aprendem o básico de todos os estilos de combate. Todo guerreiro sabe brandir um machado, esgrimir com uma rapieira, empunhar uma espada longa ou uma espada grande, usar um arco ou mesmo prender inimigos em uma rede com algum grau de perícia. Da mesma forma, um guerreiro sabe usar escudos e qualquer tipo de armadura.";
+}
+function paladin(){
+    document.getElementById('pText').innerHTML = "Um paladino jura defender a justiça e integridade, se manter com as coisas boas do mundo contra a invasão das trevas e caçar as forças do mal onde quer que ela se esconda";
+}
+function sorcerer(){
+    document.getElementById('pText').innerHTML = "A magia é parte de todo feiticeiro, inundando corpo, mente e espirito com um poder latente que espera para ser dominado. Alguns feiticeiro carregam magia que emerge de uma antiga linhagem infundida com a magia dos dragões. Outros carregam uma magia bruta, incontrolável dentro de si, uma tormenta caótica que se manifesta de formas inexplicáveis.";
+}
+function monk(){
+    document.getElementById('pText').innerHTML = "Monges fazem estudos cuidadosos da energia mágica que a maioria das tradições monásticas chama de chi. Essa energia é um elemento da mágica que inunda o multiverso – especificamente, os elementos que fluem através dos corpos vivos.";
+}
+function ranger(){
+    document.getElementById('pText').innerHTML = "Guerreiros da natureza, os patrulheiros se especializaram em caçar monstros que ameaçam as margens da civilização – assaltantes humanoides, bestas e monstruosidades devastadoras, gigantes terríveis e dragões mortais.";
+}
+
+
+
+
